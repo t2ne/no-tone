cc42b03d18522e6e1c69f755952ed0e89240ec98
t2ne <github@no-tone.com>
2025-12-21 12:52:51 +0000

    RSS?

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,31 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection("blog");
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const pages = [
+    {
+      title: "empty",
+      description: "Empty page",
+      link: "/empty/",
+      pubDate: new Date(),
+    },
+    {
+      title: "thoughts",
+      description: "Thoughts page",
+      link: "/thoughts/",
+      pubDate: new Date(),
+    },
+    {
+      title: "usr",
+      description: "Usr page",
+      link: "/usr/",
+      pubDate: new Date(),
+    },
+  ];
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: pages,
+  });
 }
